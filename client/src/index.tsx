220f9b5dc47a702e86afb0db0d7cfa66d4b0b158
QuanKA <quankieu99@gmail.com>
2023-12-21 11:18:42 +0700

    End of section 4

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,13 +7,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './app/layout/style.css'
 import App from './app/layout/App';
+import {RouterProvider} from "react-router-dom";
+import {router} from "./app/router/Routers";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <App/>
+        <RouterProvider router={router}/>
     </React.StrictMode>
 );
 
